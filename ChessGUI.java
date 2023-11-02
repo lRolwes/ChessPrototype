a7888bf04482e68425eb4b03c697cbb8a0f96ae1
Rolwes <m235370@academy.usna.edu>
2023-11-02 11:32:02 -0500

    text based pieces and a graveyard

--- ChessGUI.java
+++ ChessGUI.java
@@ -25,11 +25,38 @@ public class ChessGUI
         squares[i] = new JButton();
         squares[i].setPreferredSize(new Dimension(50,50));
         if((row+col)%2==1){
-            squares[i].setBackground(Color.BLACK);
+            squares[i].setBackground(new Color(210, 125, 45));
         }
         else{
-            squares[i].setBackground(Color.WHITE);
-
+            squares[i].setBackground(new Color(193, 154, 107));
+        }
+        if(row==1 || row==6){
+            squares[i].setText("P");
+        }
+        if(row==0||row==7){
+            if(col==0||col==7){
+                squares[i].setText("R");
+            }
+            else if(col==1||col==6){
+                squares[i].setText("K");
+            }
+            else if(col==2||col==5){
+                squares[i].setText("B");
+            }
+            else{
+                if((row+col)%2==1){
+                    squares[i].setText("K");
+                }
+                else{
+                    squares[i].setText("Q");
+                }
+            }
+        }
+        if(row>4){
+            squares[i].setForeground(Color.WHITE);
+        }
+        else{
+            squares[i].setForeground(Color.BLACK);
         }
         if(col ==7){
             col = 0;
@@ -46,9 +73,15 @@ public class ChessGUI
       this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
       this.mainPanel = new JPanel();
+      JPanel secPanel = new JPanel();
+      secPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+      secPanel.setPreferredSize(new Dimension(100,300));
+      JLabel deadPieces = new JLabel("Graveyard");
+      secPanel.add(deadPieces);
+
 
       // set the background color of the panel that contains everything
-      this.mainPanel.setBackground(new Color(227, 206, 245));
+      this.mainPanel.setBackground(new Color(194, 178, 128));
       // align the components of the main panel vertically
       this.mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
       // add some padding to the edges of the main panel
@@ -57,10 +90,9 @@ public class ChessGUI
       this.instructions = new JLabel("Welcome to ChessMaster!");
       this.instructions.setAlignmentX(Component.CENTER_ALIGNMENT);
       this.mainPanel.add(instructions);
-      mainPanel.add(board);
-
+      this.mainPanel.add(board);
+      this.mainPanel.add(secPanel);
       this.mainFrame.add(mainPanel);
-
       this.mainFrame.pack();
       this.mainFrame.setVisible(true);
    }
